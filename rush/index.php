6f9134f40fc734e1d67b8f04978df263edff908c
Charlelie BOUSSAUD <cboussau@e1r2p18.42.fr>
2016-06-17 20:44:43 +0200

    hello

---
+++ rush/index.php
@@ -0,0 +1,88 @@
+<html>
+	<head>
+		<title>Motorhead</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<a href="http://e1r2p18.42.fr:8080/rush/index.php"><div class="banner">Motorhead</div></a>
+		<div class="leftmodule">
+			<div class="blocs">Moto:</div><br /><br />
+			<ul id="menu" class="classe">
+				<li>
+					<a href="#">Marques &rsaquo;</a>
+					<ul>
+						<li>Yamaha</li>
+						<li>Guzzi</li>
+						<li>Kawasaki</li>
+						<li>Suzuki</li>
+						<li>Ducati</li>
+						<li>Bmw</li>
+						<li>Triumph</li>
+					</ul>
+				</li>
+			</ul><br />
+			<ul class="classe" id="menu">
+				<li>
+					<a href="#">Ann&eacute;es  &rsaquo;</a>
+					<ul id="year">
+						<li>- 2009 -</li>
+						<li>- 2010 -</li>
+						<li>- 2012 -</li>
+						<li>- 2014 -</li>
+						<li>- 2015 -</li>
+					</ul>
+				</li>
+			</ul><br />
+			<ul class="classe" id="menu">
+				<li>
+					<a href="#:\">Cylindr&eacute;es &rsaquo;</a>
+					<ul id="year">
+						<li>600cc</li>
+						<li>750cc</li>
+						<li>900cc</li>
+						<li>1200cc</li>
+					</ul>
+				</li>
+			</ul><br /><br />
+			<div class="blocs">Accessoires:</div><br /><br />
+			<ul class="classe" id="menu">
+				<li>
+					<a href="#">Casques &rsaquo;</a>
+					<ul>
+						<li>Caca</li>
+						<li>Caca</li>
+						<li>Caca</li>
+						<li>Caca</li>
+						<li>Caca</li>
+					</ul>
+				</li>
+			</ul><br />
+			<ul class="classe" id="menu">
+				<li>
+					<a href="#">Equipements &rsaquo;</a>
+					<ul>
+						<li>Caca</li>
+						<li>Caca</li>
+						<li>Caca</li>
+						<li>Caca</li>
+						<li>Caca</li>
+					</ul>
+				</li>
+			</ul><br /><br />
+		</div>
+		<div class="rightmodule">
+			<form action"index.php" method="POST" class="log">
+			Identifiant: <br /><input type="text" name="login" value="" placeholder="Identifiant" class="sub"><br />
+			Mot de passe: <br /><input type="password" name ="passwd" value="" placeholder="Mot de passe" class="sub"><br /><br />
+			<input type="submit" name="submit" value="Connexion" class="sub">
+			</form>
+			<div class="modif">
+				<a href="create.html" class="ident">S'inscrire</a><br /><p>Ou</p>
+				<a href="modif.html" class="ident">Modifier son mot de passe</a><br />
+			</div>
+			<div class="log"><br /><br />
+				<a href="#" class="panier">Mon panier</a>
+			</div>
+		</div>
+	</body>
+</html>
